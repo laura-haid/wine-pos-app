126a034ed28b8215da7f9b43b3a19b298868ded0
Gign Developer <andre.teixeira@gign.ca>
2021-03-23 05:36:35 -0600

    added another endpoint in the server that will handle the purchases requested by the client. Work In Progress: implement input validation, the two related usecases, and helpers for handling http response

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,14 @@
-import * as express from 'express'; 
-import * as cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+import {Request, Response} from 'express';
 
 const server = express();
 const PORT = 5000
 
 server.use(cors({origin: true}));
+server.use(bodyParser.json({}))
 
 server.get('/image', (req, res) => {
   
@@ -15,6 +19,50 @@ server.get('/image', (req, res) => {
       homeUrl: 'https://www.mastrantonio.com/wp-content/uploads/2020/05/wine-7.jpg',
       message: "Url retrieved with success"
   });
+
+})
+
+server.post('/purchase', (req: Request, res: Response) => {
+  console.log(JSON.stringify(req.body));
+  // filter request body
+  const httpRequest = {
+    body: req.body
+  }
+  // @todo handle request with a controller
+  try {
+    // @todo input validation
+    // if (!httpRequest || !httpRequest.body || !httpRequest.body.id || !httpRequest.body.token) {
+      // return badRequest(`Missing param`));
+    // }
+
+    // @todo register purchase on database
+    // const purchaseData = { productId: httpRequest.body.id, paymentInfo: httpRequest.body.token }
+    // const databasePurchaseAddResponse = await this.databasePurchaseAdd(purchaseData)
+    // @ error handling
+    // if (registerUserResponse.error) {
+    //   return badRequest(registerUserResponse.value)
+    // }
+
+    // @todo create order, place it in the queue (also collection in database)
+    // const orderData = { productId: httpRequest.body.id, orderInfo: httpRequest.body.purchaseInfo }
+    // const createOrderResponse = await this.createOrder(orderData)
+    // @ error handling
+    // if (createOrderResponse.error) {
+    //   return badRequest(createOrderResponse.value)
+    // }
+
+    const okRequest = {
+      statusCode: 200,
+      body: {message: '@todo'}
+    }
+    res.status(okRequest.statusCode).json(okRequest.body)
+  } catch (error) {
+    const serverError = {
+      statusCode: 500,
+      body: 'Server error: ' + error + '.'
+    }
+    res.status(serverError.statusCode).json(serverError.body)
+  }
 
 })
 
